6a3d19786960c123826f13ccc6f0f0fc81ea0d54
Imran M Yousuf <imyousuf@smartitengineering.com>
2009-10-26 18:10:27 +0700

    Add proxy pattern based solution for alternate to anonymous class
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- src/main/resources/com/smartitengineering/demo/wicket/groovy/HomePage.groovy
+++ src/main/resources/com/smartitengineering/demo/wicket/groovy/HomePage.groovy
@@ -5,6 +5,7 @@ import org.apache.wicket.PageParameters
 import com.smartitengineering.demo.wicket.groovy.api.GroovyWicketLink
 import com.smartitengineering.demo.wicket.groovy.api.GroovyWicketAjaxLink
 import org.apache.wicket.markup.html.link.Link
+import org.apache.wicket.ajax.markup.html.AjaxLink
 
 /**
  *
@@ -14,7 +15,9 @@ public class HomePage extends WebPage{
   public HomePage(final PageParameters parameters) {
     add(new Label("message", "If you see this message wicket is properly configured and running!"));
     add(new GroovyWicketLink("link", {link -> link.setResponsePage(HomePage.class)}));
+    add(WicketClosureFactory.getWicketLink("link1", {opName, link -> link.setResponsePage(HomePage.class)}));
     add(new GroovyWicketAjaxLink("ajaxLink", {link, target -> target.appendJavascript("alert('Imran');")}));
+    add(WicketClosureFactory.getWicketAjaxLink("ajaxLink1", {opName, link, target -> target.appendJavascript("alert('Imran Again');")}));
   }
 	public HomePage() {
     this(null);
---
+++ src/main/resources/com/smartitengineering/demo/wicket/groovy/WicketClosureFactory.groovy
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smartitengineering.demo.wicket.groovy
+import org.apache.wicket.markup.html.link.Link
+import net.sf.cglib.proxy.Enhancer
+import net.sf.cglib.proxy.CallbackFilter
+import org.apache.wicket.ajax.markup.html.AjaxLink
+
+/**
+ *
+ * @author imyousuf
+ */
+class WicketClosureFactory {
+	static Link getWicketLink(wicketId, onClickClosure) {
+    return getInstance(Link.class, wicketId, onClickClosure)
+  }
+	static AjaxLink getWicketAjaxLink(wicketId, onClickClosure) {
+    return getInstance(AjaxLink.class, wicketId, onClickClosure)
+  }
+
+  private static Object getInstance(clazz, wicketId, onClickClosure) {
+    Enhancer enhancer = new Enhancer();
+    enhancer.setCallback(new AbstractMethodHandler(onClickClosure))
+    enhancer.setSuperclass(clazz)
+    return enhancer.create([String.class].toArray(new Class[0]),
+      [wicketId].toArray(new String[0]));
+  }
+}
+
